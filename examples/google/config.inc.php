67a349ed0a32a215e0fd4ce9b01be1fead4538c7
cvs2svn <cvs2svn@php.net>
2008-11-26 10:24:11 +0000

    This commit was manufactured by cvs2svn to create branch 'RELEASE_0_99_2'.
    git-svn-id: https://svn.php.net/repository/pecl/oauth/branches/RELEASE_0_99_2@269875 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/google/config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* you need to register a consumer key at https://www.google.com/accounts/ManageDomains, after registering you are given the secret */
-define('OAUTH_CONSUMER_KEY',"oauth_consumer_key");
-define('OAUTH_CONSUMER_SECRET',"oauth_secret");
-?>

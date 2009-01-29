8ec16b6a43c1e0cc90324edb7f1f422e2f6286b3
John Jawed <jawed@php.net>
2009-01-29 22:29:26 +0000

    Update Netflix examples
    git-svn-id: https://svn.php.net/repository/pecl/oauth/trunk@274891 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/netflix/request_token.php
+++ examples/netflix/request_token.php
@@ -1,18 +1,15 @@
 <?php
-
-require 'config.inc.php';
-
+require("config.inc.php");
 try {
-	$oauth = new OAuth(OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET, OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_URI);
-	$response = $oauth->getRequestToken('http://api.netflix.com/oauth/request_token');
-	file_put_contents('/tmp/request_token_resp', serialize($response));
-	$login = $response['login_url'];
-
-	if (PHP_SAPI == 'cli') {
-		echo "Navigate your http client to: {$login}\n";
+	$o = new OAuth(OAUTH_CONSUMER_KEY,OAUTH_CONSUMER_SECRET,OAUTH_SIG_METHOD_HMACSHA1,OAUTH_AUTH_TYPE_URI);
+	$arrayResp = $o->getRequestToken("http://api.netflix.com/oauth/request_token");
+	file_put_contents("/tmp/request_token_resp",serialize($arrayResp));
+	$authorizeUrl = $arrayResp["login_url"] . "&oauth_consumer_key=" . OAUTH_CONSUMER_KEY . "&application_name=" . $arrayResp["application_name"];
+	if(PHP_SAPI=="cli") {
+		echo "Navigate your http client to: {$authorizeUrl}\n";
 	} else {
-		header("Location: {$login}");
+		header("Location: {$authorizeUrl}");
 	}
-} catch(OAuthException $e) {
-	echo "Response: ". $e->lastResponse . "\n";
+} catch(OAuthException $E) {
+	echo "Response: ". $E->lastResponse . "\n";
 }

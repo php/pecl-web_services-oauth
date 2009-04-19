92c6ab9837fc20b5b5abcaeb79acaeb4c3e8fa90
cvs2svn <cvs2svn@php.net>
2009-04-19 01:03:39 +0000

    This commit was manufactured by cvs2svn to create branch 'CUSTOMREQUEST_0_99_5'.
    git-svn-id: https://svn.php.net/repository/pecl/oauth/branches/CUSTOMREQUEST_0_99_5@278942 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/fireeagle/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* you need to register a consumer key at https://fireeagle.yahoo.net/developer, after registering you are given the secret, and the general purpose token/secret */
-define('OAUTH_CONSUMER_KEY','fookey');
-define('OAUTH_CONSUMER_SECRET','foosecret');
-define('GENERAL_PURPOSE_TOKEN','footoken');
-define('GENERAL_PURPOSE_TOKEN_SECRET','footokensecret');
-?>

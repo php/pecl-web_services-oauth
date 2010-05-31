a6ddb19091f2fc1570a3ea508a6472433fdb1979
John Jawed <jawed@php.net>
2010-05-31 03:00:31 +0000

    Update Netflix example (API request is no longer signed)
    git-svn-id: https://svn.php.net/repository/pecl/oauth/trunk@299969 c90b9560-bf6c-de11-be94-00142212c4b1

--- examples/netflix/rental_history_atom.php
+++ examples/netflix/rental_history_atom.php
@@ -6,27 +6,28 @@ require("config.inc.php");
 
 $o = new OAuth(OAUTH_CONSUMER_KEY,OAUTH_CONSUMER_SECRET,OAUTH_SIG_METHOD_HMACSHA1,OAUTH_AUTH_TYPE_AUTHORIZATION);
 try {
-	$access_token_info = unserialize(file_get_contents(OAUTH_TMP_DIR . "/access_token_resp"));
-	$o->setToken($access_token_info["oauth_token"],$access_token_info["oauth_token_secret"]);
+  $access_token_info = unserialize(file_get_contents(OAUTH_TMP_DIR . "/access_token_resp"));
+  $o->setToken($access_token_info["oauth_token"],$access_token_info["oauth_token_secret"]);
 
-	$feeds_url = "http://api.netflix.com/users/". oauth_urlencode($access_token_info["user_id"]) ."/feeds";
-	$o->fetch($feeds_url);
+  $feeds_url = "http://api.netflix.com/users/". oauth_urlencode($access_token_info["user_id"]) ."/feeds";
+  $o->fetch($feeds_url);
 
-	$feeds = $o->getLastResponse();
+  $feeds = $o->getLastResponse();
 
-    /* we need to pick the rental history feed (returned rentals) */
+  /* we need to pick the rental history feed (returned rentals) */
 
-    $feeds_xml = new SimpleXMLElement($feeds);
+  $feeds_xml = new SimpleXMLElement($feeds);
 
-    /* if you want to access other feeds, change the following rel attribute */
-    $feed_rel = "http://schemas.netflix.com/feed.rental_history.returned";
+  /* if you want to access other feeds, change the following rel attribute */
+  $feed_rel = "http://schemas.netflix.com/feed.rental_history.returned";
 
-    $returned_feed = current($feeds_xml->xpath("/resource/link[@rel=\"{$feed_rel}\"]"))->attributes();
+  $returned_feed = current($feeds_xml->xpath("/resource/link[@rel=\"{$feed_rel}\"]"))->attributes();
 
-    $o->fetch($returned_feed["href"]);
-	echo $o->getLastResponse();
+  /* don't sign the feed requests */
+  $curl = curl_init($returned_feed["href"]);
+  curl_exec($curl);
 } catch(OAuthException $E) {
-	echo "Exception caught!\n";
-	echo "Response: ". $E->lastResponse . "\n";
-    var_dump($E);
+  echo "Exception caught!\n";
+  echo "Response: ". $E->lastResponse . "\n";
+  var_dump($E);
 }
